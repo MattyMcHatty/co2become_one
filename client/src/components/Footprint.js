cdce2b39fe5498935130eefae732dc311e9ae108
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-30 15:35:52 +0000

    Started creating logic to calculate footprint score

--- client/src/components/Footprint.js
+++ client/src/components/Footprint.js
@@ -2,6 +2,49 @@ import React from 'react'
 
 const Footprint = ({footprint}) => {
 
+    const CalculateFinalScore =(footprint) =>{
+    let household_score = 0;
+    let house_size_score = 0;
+    let food_score = 0;
+    let water_score = 0;
+    let purchases_score = 0;
+    let waste_score = 0;
+    let recycle_score = 0;
+    let transport_score = 0;
+
+    switch(footprint.household){
+        case '1' : household_score = 14
+        break;
+        case '2' : household_score = 12
+        break;
+        case '3' : household_score = 10
+        break;
+        case '4' : household_score = 8
+        break; 
+        case '5' : household_score = 6
+        break;
+        case '6' : household_score = 4
+        break;
+        case '7' : household_score = 2
+        break;
+    }
+
+    switch(footprint.house_size){
+        case '1' : household_score = 14
+        break;
+        case '2' : household_score = 12
+        break;
+        case '3' : household_score = 10
+        break;
+        case '4' : household_score = 8
+        break; 
+    }
+
+    const final_score = household_score;
+    return final_score;
+}
+
+    const footprint_score = CalculateFinalScore(footprint)
 
     return (
         <>
@@ -15,7 +58,7 @@ const Footprint = ({footprint}) => {
         <div>Waste: {footprint.waste}</div>
         <div>Do You Recycle?: {footprint.recycle ? "Yes" : "No"}</div>
         <div>Transport: {footprint.transport}</div>
-        <div>Carbon Footprint Score: MAFFS</div>
+        <div>Carbon Footprint Score: {footprint_score}</div>
         <br></br>
         </div>
         </>
