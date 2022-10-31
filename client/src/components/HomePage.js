6a8520aed548358c281e9f8be6fb2c1a17b638b8
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-31 14:22:15 +0000

    added a blurb to the homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>Homepage</div>
+        <>
+        <h2>Welcome to CO2 Become One</h2>
+        <main>Calculate your carbon footprint score today and check out our handy tips to figure out how 
+            you can become one with mother nature.
+        </main>
+        <footer>
+            <h3>Our Team</h3>
+            <ul>
+                <li>
+                    Lucy Collins
+                </li>
+                <li>
+                    Matthew Arthur
+                </li>
+                <li>
+                    Colin Cameron
+                </li>
+            </ul>
+
+        </footer>
+        </>
     )
 }
 
