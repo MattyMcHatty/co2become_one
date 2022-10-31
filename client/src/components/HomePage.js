ba74b2fcde167deee69704bd252a55d5582e6ffe
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-31 14:34:11 +0000

    Added team photos to the homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
 
 const HomePage = () => {
     return (
@@ -12,12 +15,18 @@ const HomePage = () => {
             <ul>
                 <li>
                     Lucy Collins
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03U55RK01E-a8dd716e46c8-512" height="250px" width="250px"/>
                 </li>
                 <li>
                     Matthew Arthur
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3G4UX-650fe21a87dc-512" height="250px" width="250px"/>
                 </li>
                 <li>
                     Colin Cameron
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3TNET-f3a9457c7e38-512" height="250px" width="250px"/>
                 </li>
             </ul>
 
