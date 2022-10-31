6cd91fefe2601d9152b0b8d362b5e7527433f5fc
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-31 14:58:37 +0000

    styled the photos and put them in nice boxes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,10 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TeamList = styled.ul`
-list-style-type: none;
-display: flex;
-justify-content: space-around;
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
 
+`
+
+const TeamMember = styled.li`
+    color: #00802b;
+    border: 2px solid #00802b;
+    border-radius: 8px;
+    padding: 5px;
+    font-size: 1.5rem;
 `
 
 const HomePage = () => {
@@ -18,21 +26,24 @@ const HomePage = () => {
         <footer>
             <h3>Our Team</h3>
             <TeamList>
-                <li>
+                <TeamMember>
                     Lucy Collins
                     <br></br>
-                    <img src="https://ca.slack-edge.com/T0TN401HD-U03U55RK01E-a8dd716e46c8-512" height="250px" width="250px"/>
-                </li>
-                <li>
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03U55RK01E-a8dd716e46c8-512" alt="" height="250px" width="250px"/>
+                </TeamMember>
+                <TeamMember>
                     Matthew Arthur
                     <br></br>
-                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3G4UX-650fe21a87dc-512" height="250px" width="250px"/>
-                </li>
-                <li>
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3G4UX-650fe21a87dc-512" alt="" height="250px" width="250px"/>
+                </TeamMember>
+                <TeamMember>
                     Colin Cameron
                     <br></br>
-                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3TNET-f3a9457c7e38-512" height="250px" width="250px"/>
-                </li>
+                    <br></br>
+                    <img src="https://ca.slack-edge.com/T0TN401HD-U03T0T3TNET-f3a9457c7e38-512" alt="" height="250px" width="250px"/>
+                </TeamMember>
             </TeamList>
 
         </footer>
