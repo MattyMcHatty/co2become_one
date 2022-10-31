d2464cc24d23b45290a7a9eaf87fe14456ce1422
Colin Cameron <ccameron2020@gmail.com>
2022-10-31 10:47:37 +0000

    First graph

--- client/src/components/FootprintGraph.js
+++ client/src/components/FootprintGraph.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-const FootprintGraph = () => {
-    return (
-        <div>FootprintGraph</div>
-    )
-}
+const options = {
+    title: {
+      text: 'My stock chart'
+    },
+    series: [{
+      data: [1, 2, 3]
+    }]
+  }
+  
+const FootprintGraph = () =>
+
+<HighchartsReact highcharts={Highcharts} options={options} />
+
 
 export default FootprintGraph
+// constructorType={'stockChart'}
