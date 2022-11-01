18d7f99d7960fc45c1a71f982822b3fa30ccf816
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-11-01 14:56:35 +0000

    fixed some CSS

--- client/src/components/TranquilityPage.js
+++ client/src/components/TranquilityPage.js
@@ -29,7 +29,7 @@ const TranquilityPage = () => {
     
     return(
         <>
-
+        <main className="tranquil">
         <h2>Experience the tranquility of a net zero world</h2>
         <h2>One where everyone has a low carbon footprint.</h2>
         <audio controls autoPlay><source src="https://cdn.pixabay.com/download/audio/2022/02/07/audio_6666f206aa.mp3?filename=relaxing-mountains-rivers-streams-running-water-18178.mp3" type="audio/mp3" ></source></audio>
@@ -37,6 +37,7 @@ const TranquilityPage = () => {
         <br></br>
         {/* <button onClick={clickTranquilToggle}>Click Me to Find Out What Will Happen If You Don't Reduce Your Carbon Footprint</button> */}
         <Link to="/worldonfire"><button>Click Me to Find Out What Will Happen If You Don't Reduce Your Carbon Footprint</button></Link>
+        </main>
         </>
 
 
